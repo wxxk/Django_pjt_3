948c40dbcf8b9d21aa0c8fee2b3feea73af70745
wxxk <dwde22@gmail.com>
2022-10-14 14:01:17 +0900

    accounts_delete까지

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -10,3 +10,9 @@ class CustomUserCreationForm(UserCreationForm):
             "password1",
             "password2",
         )
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm):
+        model = get_user_model()
+        fields = ("username",)
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout as auth_logout
 from .models import User
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
 
@@ -20,6 +23,7 @@ def signup(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            auth_login(request, user)
             return redirect("accounts:index")
     else:
         form = CustomUserCreationForm()
@@ -54,3 +58,41 @@ def detail(request, pk):
         "user_detail": user_detail,
     }
     return render(request, "accounts/detail.html", context)
+
+
+@login_required
+def update(request, pk):
+    user = User.objects.get(pk=pk)
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:detail", user.pk)
+    else:
+        form = CustomUserChangeForm(instance=user)
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/update.html", context)
+
+
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect("accounts:index")
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/change_password.html", context)
+
+
+@login_required
+def delete(request, pk):
+    user = User.objects.get(pk=pk)
+    user.delete()
+    return redirect("accounts:index")
