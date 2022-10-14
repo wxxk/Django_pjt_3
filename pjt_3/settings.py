be737cbde247ae938cd78c627a321ae58a031966
jejoonlee <jejoonlee1996@gmail.com>
2022-10-14 10:24:56 +0900

    이제준 first settings.py + reviews 앱

--- pjt_3/settings.py
+++ pjt_3/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "accounts",
+    'reviews',
+    'django_bootstrap5',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
