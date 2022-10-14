35f808e90178727df9a3ebbce33832cc682d1aec
wxxk <dwde22@gmail.com>
2022-10-14 11:16:04 +0900

    accounts_detail까지

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as auth_logout
+from .models import User
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
 
+def index(request):
+    users = User.objects.all()
+    context = {"users": users}
+    return render(request, "accounts/index.html", context)
+
+
 def signup(request):
-    pass
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect("accounts:index")
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("accounts:index")
+    else:
+        form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("accounts:index")
+
+
+def detail(request, pk):
+    user_detail = get_user_model().objects.get(pk=pk)
+    context = {
+        "user_detail": user_detail,
+    }
+    return render(request, "accounts/detail.html", context)
